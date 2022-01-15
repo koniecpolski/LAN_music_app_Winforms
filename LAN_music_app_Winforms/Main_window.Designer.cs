6c0c3f1d74c1c0ea0eb55731b39312b8fe57bb16
koniecpolski <janpiotr8@gmail.com>
2022-01-15 16:13:04 +0100

    nie działa

--- LAN_music_app_Winforms/Main_window.Designer.cs
+++ LAN_music_app_Winforms/Main_window.Designer.cs
@@ -33,18 +33,22 @@ namespace LAN_music_app_Winforms
             this.panel_left = new System.Windows.Forms.Panel();
             this.label_connected = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.button_start_server = new System.Windows.Forms.Button();
             this.button_next = new System.Windows.Forms.Button();
             this.button_start = new System.Windows.Forms.Button();
-            this.button_start_server = new System.Windows.Forms.Button();
             this.panel_main = new System.Windows.Forms.Panel();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.button_pause = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_dodaj = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text_played = new System.Windows.Forms.TextBox();
+            this.vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
             this.label2 = new System.Windows.Forms.Label();
             this.button_set = new System.Windows.Forms.Button();
             this.textBox_czas = new System.Windows.Forms.TextBox();
-            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.list_playlist = new System.Windows.Forms.ListBox();
-            this.vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
-            this.text_played = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.panel_left.SuspendLayout();
             this.panel_main.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).BeginInit();
@@ -55,13 +59,11 @@ namespace LAN_music_app_Winforms
             this.panel_left.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.panel_left.Controls.Add(this.label_connected);
             this.panel_left.Controls.Add(this.label1);
-            this.panel_left.Controls.Add(this.button_next);
-            this.panel_left.Controls.Add(this.button_start);
             this.panel_left.Controls.Add(this.button_start_server);
             this.panel_left.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel_left.Location = new System.Drawing.Point(0, 0);
             this.panel_left.Name = "panel_left";
-            this.panel_left.Size = new System.Drawing.Size(139, 377);
+            this.panel_left.Size = new System.Drawing.Size(139, 344);
             this.panel_left.TabIndex = 0;
             // 
             // label_connected
@@ -87,18 +89,28 @@ namespace LAN_music_app_Winforms
             this.label1.TabIndex = 3;
             this.label1.Text = "Liczba połączonych";
             // 
+            // button_start_server
+            // 
+            this.button_start_server.Location = new System.Drawing.Point(32, 25);
+            this.button_start_server.Name = "button_start_server";
+            this.button_start_server.Size = new System.Drawing.Size(75, 34);
+            this.button_start_server.TabIndex = 0;
+            this.button_start_server.Text = "URUCHOM\r\nSERWER";
+            this.button_start_server.UseVisualStyleBackColor = true;
+            // 
             // button_next
             // 
-            this.button_next.Location = new System.Drawing.Point(26, 226);
+            this.button_next.Location = new System.Drawing.Point(114, 71);
             this.button_next.Name = "button_next";
             this.button_next.Size = new System.Drawing.Size(75, 23);
             this.button_next.TabIndex = 2;
             this.button_next.Text = "NEXT";
             this.button_next.UseVisualStyleBackColor = true;
+            this.button_next.Click += new System.EventHandler(this.button_next_Click);
             // 
             // button_start
             // 
-            this.button_start.Location = new System.Drawing.Point(26, 187);
+            this.button_start.Location = new System.Drawing.Point(33, 71);
             this.button_start.Name = "button_start";
             this.button_start.Size = new System.Drawing.Size(75, 23);
             this.button_start.TabIndex = 1;
@@ -106,98 +118,79 @@ namespace LAN_music_app_Winforms
             this.button_start.UseVisualStyleBackColor = true;
             this.button_start.Click += new System.EventHandler(this.button_start_Click);
             // 
-            // button_start_server
-            // 
-            this.button_start_server.Location = new System.Drawing.Point(26, 25);
-            this.button_start_server.Name = "button_start_server";
-            this.button_start_server.Size = new System.Drawing.Size(75, 23);
-            this.button_start_server.TabIndex = 0;
-            this.button_start_server.Text = "URUCHOM";
-            this.button_start_server.UseVisualStyleBackColor = true;
-            // 
             // panel_main
             // 
             this.panel_main.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel_main.Controls.Add(this.button_stop);
+            this.panel_main.Controls.Add(this.button_pause);
+            this.panel_main.Controls.Add(this.label5);
+            this.panel_main.Controls.Add(this.label4);
+            this.panel_main.Controls.Add(this.button_dodaj);
             this.panel_main.Controls.Add(this.label3);
+            this.panel_main.Controls.Add(this.button_next);
             this.panel_main.Controls.Add(this.text_played);
+            this.panel_main.Controls.Add(this.button_start);
             this.panel_main.Controls.Add(this.vlcControl1);
             this.panel_main.Controls.Add(this.label2);
             this.panel_main.Controls.Add(this.button_set);
             this.panel_main.Controls.Add(this.textBox_czas);
-            this.panel_main.Controls.Add(this.progressBar);
             this.panel_main.Controls.Add(this.list_playlist);
             this.panel_main.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel_main.Location = new System.Drawing.Point(139, 0);
             this.panel_main.Name = "panel_main";
-            this.panel_main.Size = new System.Drawing.Size(389, 377);
+            this.panel_main.Size = new System.Drawing.Size(389, 344);
             this.panel_main.TabIndex = 1;
             // 
-            // label2
+            // button_stop
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(39, 219);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Odtwarzana minuta:";
+            this.button_stop.Location = new System.Drawing.Point(278, 72);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(75, 23);
+            this.button_stop.TabIndex = 13;
+            this.button_stop.Text = "STOP";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
             // 
-            // button_set
+            // button_pause
             // 
-            this.button_set.Location = new System.Drawing.Point(253, 216);
-            this.button_set.Name = "button_set";
-            this.button_set.Size = new System.Drawing.Size(75, 23);
-            this.button_set.TabIndex = 4;
-            this.button_set.Text = "USTAW";
-            this.button_set.UseVisualStyleBackColor = true;
+            this.button_pause.Location = new System.Drawing.Point(196, 72);
+            this.button_pause.Name = "button_pause";
+            this.button_pause.Size = new System.Drawing.Size(75, 23);
+            this.button_pause.TabIndex = 12;
+            this.button_pause.Text = "PAUZA";
+            this.button_pause.UseVisualStyleBackColor = true;
+            this.button_pause.Click += new System.EventHandler(this.button_pause_Click);
             // 
-            // textBox_czas
+            // label5
             // 
-            this.textBox_czas.Location = new System.Drawing.Point(146, 216);
-            this.textBox_czas.Name = "textBox_czas";
-            this.textBox_czas.Size = new System.Drawing.Size(100, 20);
-            this.textBox_czas.TabIndex = 3;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Lista plików do odtwarzania:";
             // 
-            // progressBar
+            // label4
             // 
-            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar.Location = new System.Drawing.Point(33, 187);
-            this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(321, 23);
-            this.progressBar.TabIndex = 2;
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 26);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Aby dodać plik, przeciągnij go i upuść \r\nlub skorzystaj z przycisku:\r\n";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // list_playlist
+            // button_dodaj
             // 
-            this.list_playlist.AllowDrop = true;
-            this.list_playlist.FormattingEnabled = true;
-            this.list_playlist.Location = new System.Drawing.Point(33, 255);
-            this.list_playlist.Name = "list_playlist";
-            this.list_playlist.Size = new System.Drawing.Size(321, 95);
-            this.list_playlist.TabIndex = 1;
-            this.list_playlist.DragDrop += new System.Windows.Forms.DragEventHandler(this.list_playlist_DragDrop);
-            this.list_playlist.DragEnter += new System.Windows.Forms.DragEventHandler(this.list_playlist_DragEnter);
-            this.list_playlist.DoubleClick += new System.EventHandler(this.list_playlist_DoubleClick);
-            // 
-            // vlcControl1
-            // 
-            this.vlcControl1.BackColor = System.Drawing.Color.Black;
-            this.vlcControl1.Location = new System.Drawing.Point(372, 12);
-            this.vlcControl1.Name = "vlcControl1";
-            this.vlcControl1.Size = new System.Drawing.Size(5, 5);
-            this.vlcControl1.Spu = -1;
-            this.vlcControl1.TabIndex = 6;
-            this.vlcControl1.Text = "vlcControl1";
-            this.vlcControl1.Visible = false;
-            this.vlcControl1.VlcLibDirectory = ((System.IO.DirectoryInfo)(resources.GetObject("vlcControl1.VlcLibDirectory")));
-            this.vlcControl1.VlcMediaplayerOptions = null;
-            // 
-            // text_played
-            // 
-            this.text_played.Location = new System.Drawing.Point(33, 45);
-            this.text_played.Name = "text_played";
-            this.text_played.ReadOnly = true;
-            this.text_played.Size = new System.Drawing.Size(214, 20);
-            this.text_played.TabIndex = 7;
+            this.button_dodaj.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_dodaj.Location = new System.Drawing.Point(279, 301);
+            this.button_dodaj.Name = "button_dodaj";
+            this.button_dodaj.Size = new System.Drawing.Size(75, 23);
+            this.button_dodaj.TabIndex = 9;
+            this.button_dodaj.Text = "DODAJ";
+            this.button_dodaj.UseVisualStyleBackColor = true;
+            this.button_dodaj.Click += new System.EventHandler(this.button_dodaj_Click);
             // 
             // label3
             // 
@@ -208,11 +201,79 @@ namespace LAN_music_app_Winforms
             this.label3.TabIndex = 8;
             this.label3.Text = "Odtwarzany utwór:";
             // 
+            // text_played
+            // 
+            this.text_played.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_played.Location = new System.Drawing.Point(33, 45);
+            this.text_played.Name = "text_played";
+            this.text_played.ReadOnly = true;
+            this.text_played.Size = new System.Drawing.Size(321, 20);
+            this.text_played.TabIndex = 7;
+            // 
+            // vlcControl1
+            // 
+            this.vlcControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vlcControl1.BackColor = System.Drawing.Color.Black;
+            this.vlcControl1.Location = new System.Drawing.Point(372, 12);
+            this.vlcControl1.Name = "vlcControl1";
+            this.vlcControl1.Size = new System.Drawing.Size(5, 5);
+            this.vlcControl1.Spu = -1;
+            this.vlcControl1.TabIndex = 6;
+            this.vlcControl1.Text = "vlcControl1";
+            this.vlcControl1.Visible = false;
+            this.vlcControl1.VlcLibDirectory = ((System.IO.DirectoryInfo)(resources.GetObject("vlcControl1.VlcLibDirectory")));
+            this.vlcControl1.VlcMediaplayerOptions = null;
+            this.vlcControl1.EndReached += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerEndReachedEventArgs>(this.vlcControl1_EndReached);
+            this.vlcControl1.Stopped += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerStoppedEventArgs>(this.vlcControl1_Stopped);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Odtwarzana minuta:";
+            // 
+            // button_set
+            // 
+            this.button_set.Location = new System.Drawing.Point(142, 127);
+            this.button_set.Name = "button_set";
+            this.button_set.Size = new System.Drawing.Size(75, 23);
+            this.button_set.TabIndex = 4;
+            this.button_set.Text = "USTAW";
+            this.button_set.UseVisualStyleBackColor = true;
+            // 
+            // textBox_czas
+            // 
+            this.textBox_czas.Location = new System.Drawing.Point(36, 130);
+            this.textBox_czas.Name = "textBox_czas";
+            this.textBox_czas.Size = new System.Drawing.Size(100, 20);
+            this.textBox_czas.TabIndex = 3;
+            // 
+            // list_playlist
+            // 
+            this.list_playlist.AllowDrop = true;
+            this.list_playlist.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list_playlist.FormattingEnabled = true;
+            this.list_playlist.Location = new System.Drawing.Point(33, 197);
+            this.list_playlist.Name = "list_playlist";
+            this.list_playlist.Size = new System.Drawing.Size(321, 95);
+            this.list_playlist.TabIndex = 1;
+            this.list_playlist.DragDrop += new System.Windows.Forms.DragEventHandler(this.list_playlist_DragDrop);
+            this.list_playlist.DragEnter += new System.Windows.Forms.DragEventHandler(this.list_playlist_DragEnter);
+            this.list_playlist.DoubleClick += new System.EventHandler(this.list_playlist_DoubleClick);
+            this.list_playlist.KeyUp += new System.Windows.Forms.KeyEventHandler(this.list_playlist_KeyUp);
+            // 
             // Main_window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 377);
+            this.ClientSize = new System.Drawing.Size(528, 344);
             this.Controls.Add(this.panel_main);
             this.Controls.Add(this.panel_left);
             this.Name = "Main_window";
@@ -236,7 +297,6 @@ namespace LAN_music_app_Winforms
         private System.Windows.Forms.Button button_next;
         private System.Windows.Forms.Button button_start;
         private System.Windows.Forms.Button button_start_server;
-        private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.ListBox list_playlist;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button_set;
@@ -244,6 +304,11 @@ namespace LAN_music_app_Winforms
         private Vlc.DotNet.Forms.VlcControl vlcControl1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox text_played;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_dodaj;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Button button_pause;
     }
 }
 
--- LAN_music_app_Winforms/Main_window.cs
+++ LAN_music_app_Winforms/Main_window.cs
@@ -6,8 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 // https://github.com/ZeBobo5/Vlc.DotNet/wiki/Getting-started
@@ -16,26 +15,137 @@ namespace LAN_music_app_Winforms
 {
     public partial class Main_window : Form
     {
+        private int? odtwarzany = null; // nullable integer
+        //private bool next = false;
+
         public Main_window()
         {
             InitializeComponent();
         }
 
-        private void Play(string path) // funkcja ODTWARZANIA pliku dźwiękowego
+        private void vlc_Play(FileInfo plik = null)
         {
-            System.IO.FileInfo plik = new System.IO.FileInfo(path); // konwersja ścieżki na format pliku
-            string nazwa = Path.GetFileName(path); // wydobycie nazwy pliku
-            text_played.Text = nazwa; // wypisanie nazwy pliku na ekranie
-            vlcControl1.Play(plik); // włączenie odtwarzania pliku
+            if (this.InvokeRequired)
+            {
+                Invoke((Action)delegate { vlc_Play(plik); });
+            }
+            else
+            {
+                if (plik == null)
+                    vlcControl1.Play();
+                else
+                {
+                    try
+                    {
+                        vlcControl1.Play(plik);
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.ToString());
+                    }
+                }
+                    
+            }
+
+        }
+
+        private void vlc_Stop()
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke((Action)delegate { vlc_Stop(); });
+            }
+            else
+            {
+                vlcControl1.Stop();
+            }
+        }
+
+        private void vlc_Pause()
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke((Action)delegate { vlc_Pause(); });
+            }
+            else
+            {
+                vlcControl1.Pause();
+            }
+        }
+
+        public void Play_i(int index)
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke((Action)delegate { Play_i(index); });
+            }
+            else
+            {
+                if (list_playlist.Items.Count > index) // Jeżeli indeks ma sens
+                {
+                    odtwarzany = index;
+                    list_playlist.SelectedIndex = index;
+                    Play(list_playlist.Items[index].ToString()); // odtwórz plik o tym indeksie
+                }
+                else
+                    vlc_Stop();//vlcControl1.Stop();
+            }
+        }
+
+        public void Play(string path) // funkcja ODTWARZANIA pliku dźwiękowego
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke((Action)delegate { Play(path); });
+            }
+            else
+            {
+                FileInfo plik = new System.IO.FileInfo(path); // konwersja ścieżki na format pliku
+                string nazwa = Path.GetFileName(path); // wydobycie nazwy pliku
+                text_played.Text = nazwa; // wypisanie nazwy pliku na ekranie
+                vlc_Play(plik);//vlcControl1.Play(plik); // włączenie odtwarzania pliku
+            }
+        }
+
+        public void Play_next()
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke((Action)delegate { Play_next(); });
+            }
+            else
+            {
+                int? nowy;
+                if (odtwarzany != null)
+                    nowy = odtwarzany + 1;
+                else
+                    nowy = 0;
+
+                Play_i((int)nowy);
+            }
+        }
+
+        public void Clear()
+        {
+            if (this.InvokeRequired)
+                Invoke((Action)delegate { Clear(); });
+            else
+            {
+                odtwarzany = null;
+                list_playlist.ClearSelected();
+                text_played.Text = "";
+            }
         }
 
         private void button_start_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog(); // OKNO wybierania pliku
-            ofd.ShowDialog(); // pokaż OKNO
-
-            if (ofd.FileName!="") // jeżeli wybrano jakiś plik - odtwórz go
-                Play(ofd.FileName);
+            if (list_playlist.Items.Count > 0)
+            {
+                if (list_playlist.SelectedItem != null) // Jeżeli jakiś element jest wybrany
+                    Play_i(list_playlist.SelectedIndex); // Odtwórz go
+                else
+                    Play_i(0);
+            }
         }
 
         private void list_playlist_DragDrop(object sender, DragEventArgs e)
@@ -56,5 +166,57 @@ namespace LAN_music_app_Winforms
             if (list_playlist.SelectedItem != null) // Jeżeli jakiś element jest wybrany
                 Play(list_playlist.SelectedItem.ToString()); // Odtwórz go
         }
+
+        private void button_dodaj_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog(); // OKNO wybierania pliku
+            ofd.ShowDialog(); // pokaż OKNO
+
+            if (ofd.FileName != "") // jeżeli wybrano jakiś plik - odtwórz go
+                list_playlist.Items.Add(ofd.FileName);
+        }
+
+        private void button_next_Click(object sender, EventArgs e)
+        {
+            Play_next();
+        }
+
+        private void list_playlist_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+                list_playlist.Items.RemoveAt(list_playlist.SelectedIndex);
+        }
+
+        private void button_pause_Click(object sender, EventArgs e)
+        {
+            if (vlcControl1.IsPlaying)
+            {
+                button_pause.Text = "WZNÓW";
+                vlc_Pause();//vlcControl1.Pause();
+            }
+            else
+            {
+                button_pause.Text = "PAUZA";
+                vlc_Play();//vlcControl1.Play();
+            }
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            vlc_Stop();//vlcControl1.Stop();
+        }
+
+        private void vlcControl1_Stopped(object sender, Vlc.DotNet.Core.VlcMediaPlayerStoppedEventArgs e)
+        {
+            //MessageBox.Show("Playlista się skończyła");
+            (Application.OpenForms["Main_window"] as Main_window).Clear();
+        }
+
+        private void vlcControl1_EndReached(object sender, Vlc.DotNet.Core.VlcMediaPlayerEndReachedEventArgs e)
+        {
+            //base.Invoke((Action)delegate { Play_next(); });
+            (Application.OpenForms["Main_window"] as Main_window).Play_next();
+        }
+
     }
 }
